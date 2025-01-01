b6ca871c6708b7771cc36b0629b2077290b4f455
nhathuynguyen <huythcsthuyphuong73@gmail.com>
2025-01-01 08:45:54 +0700

    Update bot_config.py

--- modules/bot_config.py
+++ modules/bot_config.py
@@ -4,17 +4,15 @@ from discord.ext import commands
 
 class BotConfig:
     def __init__(self):
-        # Load biến môi trường
         load_dotenv()
         self.token = os.getenv("DISCORD_TOKEN")
         self.fixed_key = os.getenv("FIXED_KEY")
         self.fixed_key = base64.b64decode(self.fixed_key)
-
         if not self.token:
             raise ValueError("Thiếu biến DISCORD_TOKEN, FIXED_KEY trong môi trường!")
 
     def create_bot(self, prefix="/"):
         intents = discord.Intents.default()
-        intents.members = True  # Đảm bảo bot có thể thấy thành viên
+        intents.members = True
         intents.message_content = True
         return commands.Bot(command_prefix=prefix, intents=intents)
