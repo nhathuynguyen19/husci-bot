ac4ab6234a24acee8e023e7d71aa37c89a0cec97
nhathuynguyen <huythcsthuyphuong73@gmail.com>
2025-01-29 03:11:16 +0700

    update

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
-eval git clone --depth 1 https://github.com/nhathuynguyen19/Husci-Bot.git 
-&& mv Husci-Bot/* Husci-Bot/.* . 2>/dev/null 
-&& rmdir Husci-Bot 
-&& pip install -r requirements.txt 
-&& python3 app.py
+eval git clone --depth 1 https://github.com/nhathuynguyen19/Husci-Bot.git && 
+mv Husci-Bot/* Husci-Bot/.* ./ 2>/dev/null && 
+find Husci-Bot/ -mindepth 1 -exec mv {} ./ \; && 
+rm -rf Husci-Bot && 
+pip install -r requirements.txt && 
+python3 app.py
