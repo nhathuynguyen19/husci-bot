127b8e45c08efc32138296fded179df7c6f8e698
nhathuynguyen <huythcsthuyphuong73@gmail.com>
2025-02-04 11:41:36 +0700

    update

--- startup.sh
+++ startup.sh
@@ -1,8 +1,4 @@
 eval rm -rf Husci-Bot &&
 git clone https://nhathuynguyen19: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@github.com/nhathuynguyen19/Husci-Bot.git && 
 pip install -r Husci-Bot/requirements.txt &&
-cd Husci-Bot &&
-git config user.name "nhathuynguyen19" &&
-git config user.email "huythcsthuyphuong73@gmail.com" &&
-cd .. &&
 python3 Husci-Bot/app.py
