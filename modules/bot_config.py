ec6686a2a3004356229a42ac24262e8b6cde5565
nhathuynguyen <huythcsthuyphuong73@gmail.com>
2025-02-14 14:18:18 +0700

    Update bot configuration to use dotenv path and log warnings for missing dependencies

--- modules/bot_config.py
+++ modules/bot_config.py
@@ -2,18 +2,11 @@ import os, discord, base64, asyncio
 from dotenv import load_dotenv
 from discord.ext import commands
 from config import logger
-<<<<<<< HEAD
-
-class BotConfig:
-    def __init__(self):
-        load_dotenv()
-=======
 from paths import dotenv_path
 
 class BotConfig:
     def __init__(self):
         load_dotenv(dotenv_path)
->>>>>>> e23f034 (update)
         self.token = os.getenv("DISCORD_TOKEN")
         self.fixed_key = os.getenv("FIXED_KEY")
         self.fixed_key = base64.b64decode(self.fixed_key)
