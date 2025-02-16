13b9c46094c01edd6f6efd6b72ecba70068c7c0c
nhathuynguyen <huythcsthuyphuong73@gmail.com>
2025-02-16 07:30:20 +0700

    Update GitHub token in startup script for cloning repository

--- startup.bat
+++ startup.bat
@@ -1,5 +1,5 @@
 eval rm -rf Husci-Bot &&
-git clone https://nhathuynguyen19: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@github.com/nhathuynguyen19/Husci-Bot.git && 
+git clone https://nhathuynguyen19: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@github.com/nhathuynguyen19/Husci-Bot.git && 
 pip install -r Husci-Bot/requirements.txt &&
 cd Husci-Bot &&
 git config user.name "nhathuynguyen19" &&
