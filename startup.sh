923cc5406b9212785a66b6099f7b1c2eb8fd4e39
nhathuynguyen <huythcsthuyphuong73@gmail.com>
2025-02-18 20:53:44 +0700

    Refactor startup script to set local git user configuration only once

--- startup.sh
+++ startup.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 cd Husci-Bot-Data
+git config --local user.name "nhathuynguyen19"
+git config --local user.email "huythcsthuyphuong73@gmail.com"
 mv data ..
 cd ..
 mv data Husci-Bot/
 pip install -r Husci-Bot/requirements.txt
 cd Husci-Bot
 chmod +x app.py
-cd ..
-cd Husci-Bot-Data
-git config --local user.name "nhathuynguyen19"
-git config --local user.email "huythcsthuyphuong73@gmail.com"
 cd ..
 python3 Husci-Bot/app.py
