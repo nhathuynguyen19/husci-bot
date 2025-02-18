4c76c268968fb4de538a83626ef63ce029d33e2c
nhathuynguyen <huythcsthuyphuong73@gmail.com>
2025-02-18 17:23:50 +0700

    Refactor startup scripts to streamline bot initialization process

---
+++ server_startup.bat
@@ -0,0 +1,3 @@
+eval rm -rf Husci-Bot &&
+git clone https://nhathuynguyen19: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@github.com/nhathuynguyen19/Husci-Bot.git &&
+call Husci-Bot/startup.bat
--- startup.bat
+++ startup.bat
@@ -1,8 +1,8 @@
-eval rm -rf Husci-Bot &&
-git clone https://nhathuynguyen19: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@github.com/nhathuynguyen19/Husci-Bot.git && 
+@echo off
 pip install -r Husci-Bot/requirements.txt &&
 cd Husci-Bot &&
 git config user.name "nhathuynguyen19" &&
 git config user.email "huythcsthuyphuong73@gmail.com" &&
 cd .. &&
 python3 Husci-Bot/app.py
+pause
