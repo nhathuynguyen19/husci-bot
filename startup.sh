cb6c9c64dee99fc2b70486abcb4959e4de302c78
nhathuynguyen <huythcsthuyphuong73@gmail.com>
2025-02-18 18:58:21 +0700

    Set git user configuration in startup script for Husci-Bot-Data directory

--- startup.sh
+++ startup.sh
@@ -9,4 +9,8 @@ git config user.name "nhathuynguyen19"
 git config user.email "huythcsthuyphuong73@gmail.com"
 chmod +x app.py
 cd ..
+cd Husci-Bot-Data
+git config user.name "nhathuynguyen19"
+git config user.email "huythcsthuyphuong73@gmail.com"
+cd ..
 python3 Husci-Bot/app.py
