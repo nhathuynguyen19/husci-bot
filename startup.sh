9573f45a2525dab915d85139375ed11012350bd4
nhathuynguyen <huythcsthuyphuong73@gmail.com>
2025-02-18 17:32:01 +0700

    Convert startup script to Unix shell format and set executable permissions for app.py

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
-@echo off
+#!/bin/bash
 pip install -r Husci-Bot/requirements.txt
 cd Husci-Bot
 git config user.name "nhathuynguyen19"
 git config user.email "huythcsthuyphuong73@gmail.com"
+chmod +x app.py
 cd ..
 python3 Husci-Bot/app.py
-pause
