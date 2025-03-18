b4ff1db2757f0a187d5a27940077e9922291f09d
nhathuynguyen19 <huythcsthuyphuong73@gmail.com>
2025-03-18 22:25:26 +0700

    Update startup script to include system package updates and install Python 3.13

--- startup.sh
+++ startup.sh
@@ -5,6 +5,8 @@ git config --local user.email "huythcsthuyphuong73@gmail.com"
 mv data ..
 cd ..
 mv data husci-bot/
+sudo apt update && sudo apt upgrade /
+sudo apt install python3.13 /
 pip install -r husci-bot/requirements.txt
 cd husci-bot
 chmod +x app.py
